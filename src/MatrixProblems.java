50591aeffed637e4ad4d447862e85aa17143a486
Sahib <sahib.ptu@gmail.com>
2026-01-12 16:53:03 +0530

    matrix problem

--- src/MatrixProblems.java
+++ src/MatrixProblems.java
@@ -32,7 +32,7 @@ public class MatrixProblems {
           endRow--;
       }
 
-    };
+    }
     public static void main (String[]args){
         int [][] matrix = {{1,2,3,4},
                 {5,6,7,8},
