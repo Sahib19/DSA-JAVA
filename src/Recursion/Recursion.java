04e6bc028e973162fa7668b3a6e4afe675d16425
Sahib <sahib.ptu@gmail.com>
2026-01-19 18:24:42 +0530

    Recursion Problems

---
+++ src/Recursion/Recursion.java
@@ -0,0 +1,81 @@
+package Recursion;
+
+public class Recursion {
+    public static void printDec(int n){
+        if(n==1){
+            System.out.println(n);
+            return ;
+
+        }
+        System.out.print(n+" ");
+        printDec(n-1);
+    }
+
+    public static void printInc(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return ;
+
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
+
+    public static int factorial(int n){
+        if(n==1){
+            return n;
+        }else{
+            return n*factorial(n-1);
+        }
+    }
+
+    public static int SumOfN(int n){
+        if(n==1){
+            return 1;
+        }else{
+            return n+ SumOfN(n-1);
+        }
+    }
+
+    public static int fibonacciNthTerm(int n){
+        if(n==0 || n==1){
+            return n ;
+        }else{
+            return fibonacciNthTerm(n-1)+fibonacciNthTerm(n-2);
+        }
+    }
+
+    public static boolean checkSortedArray( int [] arr , int i ){
+        if(i == arr.length-1){
+            return true;
+        }
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+        return checkSortedArray(arr,i+1);
+    }
+
+    public static int firstOccurrence(int [] arr , int i , int key){
+        if(i == arr.length){
+            return -1;
+        }
+        if(arr[i] == key){
+            return i;
+        }
+        return firstOccurrence(arr,i+1,key);
+    }
+
+    public static int lastOccurrence(int [] arr , int i , int key){
+        if(i == -1){
+            return i;
+        }
+        if(arr[i] == key){
+            return i;
+        }
+        return lastOccurrence(arr,i-1,key);
+    }
+    public static void main(String[] args ){
+        int [] arr = {1,4,5,1,5,9,9};
+        System.out.println(lastOccurrence(arr,arr.length-1,5));
+    }
+}
