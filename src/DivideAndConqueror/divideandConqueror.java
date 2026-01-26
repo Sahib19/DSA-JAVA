d13e8c56ab2c721b6bfd2d96f7f9a4f98a1cdb56
Sahib <sahib.ptu@gmail.com>
2026-01-26 23:39:13 +0530

    Quick Sort

--- src/DivideAndConqueror/divideandConqueror.java
+++ src/DivideAndConqueror/divideandConqueror.java
@@ -49,9 +49,42 @@ public class divideandConqueror {
         }
     }
 
+    public static void quickSort(int arr[] , int si , int ei){
+        if(si>= ei ){
+            return;
+        }
+
+        int pIdx = partition(arr,si,ei);
+        quickSort(arr,si,pIdx-1); // left
+        quickSort(arr,pIdx+1,ei); // right
+    }
+
+    public static int partition(int [] arr, int si , int ei){
+        int pivot = arr[ei];
+        int i = si-1;
+
+        for(int j = si ; j < ei ; j++){
+            if(arr[j] <=pivot){
+                i++;
+                //swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        // place pivot at correct position
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[ei];
+        arr[ei] = temp;
+
+        return i;
+    }
+
     public static void main(String[] args){
-        int[] arr = {1,5,9,3,4};
-        mergeSort(arr,0, arr.length-1);
+        int[] arr = {1,5,9,3,4,-2};
+        quickSort(arr, 0 , arr.length-1);
         printarr(arr);
     }
 
