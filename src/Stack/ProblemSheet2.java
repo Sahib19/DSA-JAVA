2724d747171ae7b24e4c5bd9600654ca3484cc72
Sahib <sahib.ptu@gmail.com>
2026-02-21 23:40:09 +0530

    Max Area in Histogram

---
+++ src/Stack/ProblemSheet2.java
@@ -0,0 +1,14 @@
+package Stack;
+
+public class ProblemSheet2 {
+    // Max Area In a Histogram
+
+    public static void NextMinFromRight(int [ ] nums ,  int [] rightMinArr){
+        Stack <Integer> st = new Stack
+    }
+
+    public static void main(String [] args){
+        System.out.println("Histogram program ");
+
+    }
+}
