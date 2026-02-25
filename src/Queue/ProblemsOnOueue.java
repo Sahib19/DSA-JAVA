f04c76e6e6a591ed76bb69d76331a6beddc8a885
Sahib <sahib.ptu@gmail.com>
2026-02-25 16:52:57 +0530

    InterLeaving of Queue

--- src/Queue/ProblemsOnOueue.java
+++ src/Queue/ProblemsOnOueue.java
@@ -1,34 +1,55 @@
 package Queue;
+
 import java.util.*;
 import java.util.Queue;
 
 public class ProblemsOnOueue {
 
     // O(N)
-    public static void NonrepeatingChar(String str){
-        Queue<Character> q1 = new LinkedList <>();
-        int [] freq = new int [26];
+    public static void NonrepeatingChar(String str) {
+        Queue<Character> q1 = new LinkedList<>();
+        int[] freq = new int[26];
 
 
-        for( int i = 0 ; i< str.length() ; i++){
-            char ch  =  str.charAt(i);
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
             // Incrementing this char in freq array
-             freq[ch - 'a']++;
-             q1.add(ch);
-             while(!q1.isEmpty() && freq[q1.peek() - 'a'] > 1 ){
-                    q1.remove();
-             }
-             if(q1.isEmpty()){
-                 System.out.print(-1+" ");
-             }else{
-                 System.out.print(q1.peek()+" ");
+            freq[ch - 'a']++;
+            q1.add(ch);
+            while (!q1.isEmpty() && freq[q1.peek() - 'a'] > 1) {
+                q1.remove();
+            }
+            if (q1.isEmpty()) {
+                System.out.print(-1 + " ");
+            } else {
+                System.out.print(q1.peek() + " ");
 
-             }
+            }
 
         }
     }
 
-    public static void main(String [] args){
-        NonrepeatingChar("aabccxb");
+    public static void InterleavingQueue(Queue<Integer> q1) {
+        int size = q1.size();
+        Queue<Integer> q2 = new LinkedList<>();
+
+        while (q1.size() != size / 2) {
+            q2.add(q1.remove());
+        }
+
+        while (!q2.isEmpty()) {
+            q1.add(q2.remove());
+            q1.add(q1.remove());
+        }
+    }
+
+    public static void main(String[] args) {
+
+//        NonrepeatingChar("aabccxb");
+        Queue<Integer> q1 = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+//        InterleavingQueue(q1);
+//        System.out.println(q1);
+
+
     }
 }
