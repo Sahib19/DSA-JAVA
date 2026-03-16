be540b89874b6bea16ae8d11080bc6b489801e45
Sahib <sahib.ptu@gmail.com>
2026-03-16 13:43:41 +0530

    Level Order Transversal

--- src/BinaryTrees/Sheet1.java
+++ src/BinaryTrees/Sheet1.java
@@ -64,6 +64,74 @@ public class Sheet1 {
         }
     }
 
+    // DFS transversal Techniques - post , in , pre - order
+
+    public static void postOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data+" ");
+    }
+
+    public static void inOrder(Node root){
+        if(root == null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
+
+
+    public static void  preOrder (Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+
+    }
+
+    // BFS transversal Techniques - level order Technique
+    public static void levelOrderTraversal(Node root) {
+
+        Queue<Node> q = new LinkedList<>();
+
+        q.add(root);
+        q.add(null);   // level separator
+
+        while (!q.isEmpty()) {
+
+            Node currNode = q.remove();
+
+            if (currNode == null) {
+
+                System.out.println(); // move to next level
+
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+
+            } else {
+
+                System.out.print(currNode.data + " ");
+
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -73,6 +141,14 @@ public class Sheet1 {
 
         System.out.println("Root Node: " + root.data);
 
-        BinaryTreePrinter.printTree(root, 0);
+//        BinaryTreePrinter.printTree(root, 0);
+        preOrder(root);
+        System.out.println();
+        postOrder(root);
+        System.out.println();
+        inOrder(root);
+        System.out.println();
+        //
+        levelOrderTraversal(root);
     }
 }
