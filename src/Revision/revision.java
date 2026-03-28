1cc027506b8f6473b91677e3147b5033ceda3bcb
Sahib <sahib.ptu@gmail.com>
2026-03-28 16:54:35 +0530

    Kadane Revision

--- src/Revision/revision.java
+++ src/Revision/revision.java
@@ -1,17 +1,21 @@
 package Revision;
 
 public class revision {
-    public static void main(String [] args) {
-      int arr [] = {2,4,6,8,10};
+    public static void main(String[] args) {
+        int arr[] = {1, -2, 6, -1, 3};
+        int max = Integer.MIN_VALUE;
 
-      for(int i = 0 ; i < arr.length ; i++){
-          for(int j = i+1 ; j < arr.length ; j++ ){
-              for( int k = i ; k <= j ; k++){
-                  System.out.print(arr[k]+" ");
-              }
-                System.out.print(",");
-          }
-          System.out.println();
-      }
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if(max < sum){
+                max =sum ;
+            }
+            if(sum <= 0 ){
+                sum = 0 ;
+            }
+        }
+        System.out.println(max);
     }
 }
