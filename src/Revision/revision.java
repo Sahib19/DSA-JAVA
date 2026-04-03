a6578da03ee51c9d8c91cf9477c162b03659d39a
Sahib <sahib.ptu@gmail.com>
2026-04-03 13:04:11 +0530

    To Upper Case Problem revision

--- src/Revision/revision.java
+++ src/Revision/revision.java
@@ -1,49 +1,45 @@
 package Revision;
 
 public class revision {
-    public static void PrintSpiral(int [] [] arr){
-       int sr = 0 ;
-       int sc = 0 ;
-       int er = arr.length-1;
-       int ec = arr[0].length-1;
-       System.out.println(er+" "+ec);
+    public static String tuc(String str) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Character.toUpperCase(str.charAt(0)));
 
-       while( sr <= er && sc <= ec){
-
-           // Top row
-           for(int i = sc ; i <= ec ; i++){
-               System.out.print(arr[sr][i]+" ");
-           }
-
-           // right column
-           for(int j =  sr+1 ; j <= er ; j++ ){
-               System.out.print(arr[j][ec]+" ");
-           }
-
-           //bottom row
-           for( int p = ec-1 ; p >= sc ; p-- ){
-               if(sr == er) break ;
-               System.out.print(arr[er][p]+" ");
-           }
-
-           // left column
-           for( int q = er-1 ; q >= sr+1 ; q--){
-               if(sc == ec) break;
-               System.out.print(arr[q][sc]+" ");
-           }
-
-           sr++;
-           sc++;
-           er--;
-           ec--;
-       }
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && i < str.length() - 1) {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 
+    public static String tuc2(String str) {
+        boolean flag = false;
+        StringBuilder sb = new StringBuilder();
+        sb.append(Character.toUpperCase(str.charAt(0)));
+        for( int i = 1 ; i < str.length() ; i++){
+            char curr = str.charAt(i);
+            if(curr == ' '){
+                flag = true ;
+                sb.append(curr);
+            }else{
+                if(flag){
+                    sb.append(Character.toUpperCase(curr));
+                    flag = false;
+                }else{
+                    sb.append(curr);
+                }
+            }
+        }
+
+        return sb.toString();
+    }
 
     public static void main(String[] args) {
-        int[][] arr = { {1, 2, 3, 4}
-                       };
-        PrintSpiral(arr);
 
 
 
