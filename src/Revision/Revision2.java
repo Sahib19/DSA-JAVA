f8fb6ccadfb43926de82802e705aa654f02e8f09
Sahib <sahib.ptu@gmail.com>
2026-04-11 17:35:49 +0530

    Rat in Maze problem Revision

--- src/Revision/Revision2.java
+++ src/Revision/Revision2.java
@@ -11,71 +11,39 @@ public class Revision2 {
         }
     }
 
-    public static boolean SudokuSolver( int [][] sudo , int row , int col){
-        if(row == 9 ) {
-            printBoard(sudo);
-            return true;
+    public static void ratMat(int[][] maze , int row , int col , String path){
+        if(row == maze.length-1 && col == maze[0].length-1){
+            // solution tak pauch gaye ham
+            System.out.println(path+"( "+row+","+col+" )");
+            return;
+        }
+        if(row < 0 || row >= maze.length || col < 0 || col >= maze[0].length){
+            // cancel this solution
+            return;
+        }
+        if(maze[row][col] == 0){
+            return;
         }
 
+        maze[row][col] = 0;
 
-        int nextRow = row, nextCol =  col+1;
-        if( nextCol == 9){
-            nextRow = row+1;
-            nextCol = 0 ;
-        }
-         if(sudo[row][col] != 0 ){
-             return SudokuSolver(sudo, nextRow , nextCol);
-         }
+        ratMat(maze,row,col+1 , path+"( "+row+","+col+" )");
+        ratMat(maze,row+1,col ,path+"( "+row+","+col+" )");
+        ratMat(maze,row-1,col ,path+"( "+row+","+col+" )");
+        ratMat(maze,row,col-1 ,path+"( "+row+","+col+" )");
 
-        for( int digit  = 1 ; digit <= 9 ; digit++){
-            if( isSafe(sudo , row , col , digit)){
-                sudo[row][col] = digit;
-                if(SudokuSolver(sudo,nextRow , nextCol)){
-                    return true;
-                };
-                sudo[row][col] = 0 ;
-            }
-        }
-        return false;
+        maze[row][col] = 1;
+
     }
 
-    public static boolean isSafe(int[][]sudo , int row , int col , int digit){
-        // column wise
-        for( int i  = 0 ; i < 9  ; i++ ){
-            if(sudo[i][col] == digit) return false;
-        }
-
-        // row wise
-        for( int i  = 0 ; i < 9  ; i++ ){
-            if(sudo[row][i] == digit) return false;
-        }
-
-        //grid chek
-        int sr = (row/3)*3;
-        int sc = (col/3)*3;
-
-        for( int i = sr ; i < sr+3 ; i++){
-            for( int j = sc  ; j < sc+3 ; j++){
-                if(sudo[i][j] == digit) return false;
-            }
-        }
-
-        return true;
-    }
 
     public static void main(String[] args) {
-        int [][] sudo = {
-                {0, 0, 8, 0, 0, 0, 0, 0, 0},
-                {4, 9, 0, 1, 5, 7, 0, 0, 2},
-                {0, 0, 3, 0, 0, 4, 1, 9, 0},
-                {1, 8, 5, 0, 6, 0, 0, 2, 0},
-                {0, 0, 0, 0, 2, 0, 0, 6, 0},
-                {9, 6, 0, 4, 0, 5, 3, 0, 0},
-                {0, 3, 0, 0, 7, 2, 0, 0, 4},
-                {0, 4, 9, 0, 3, 0, 0, 5, 7},
-                {8, 2, 7, 0, 0, 9, 0, 1, 3}
+        int[][] maze = {
+                {1, 0, 0, 0},
+                {1, 1, 0, 1},
+                {0, 1, 0, 0},
+                {1, 1, 1, 1}
         };
-
-        SudokuSolver(sudo,0,0);
+        ratMat(maze,0,0,"");
     }
 }
