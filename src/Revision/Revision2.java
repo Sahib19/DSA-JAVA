e31fc96d3a0a4814da7f01566a3b78fc574e3b6b
Sahib <sahib.ptu@gmail.com>
2026-04-11 16:41:27 +0530

    Sudo Solver Revision

--- src/Revision/Revision2.java
+++ src/Revision/Revision2.java
@@ -1,7 +1,8 @@
 package Revision;
 
+
 public class Revision2 {
-    public static void printBoard(char[][] board) {
+    public static void printBoard(int [][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 System.out.print(board[i][j] + " ");
@@ -10,44 +11,52 @@ public class Revision2 {
         }
     }
 
-    public static boolean nQueens(int row, char[][] board) {
-        if (row == board.length) {
-            System.out.println("------ Chess Board ------");
-            printBoard(board);
+    public static boolean SudokuSolver( int [][] sudo , int row , int col){
+        if(row == 9 ) {
+            printBoard(sudo);
             return true;
         }
 
-        for (int i = 0; i < board[0].length; i++) {
-            if (isSafe(board, row, i)) {
-                board[row][i] = 'Q';
-               if( nQueens(row + 1, board)) return true ;
-                board[row][i] = 'X';
 
+        int nextRow = row, nextCol =  col+1;
+        if( nextCol == 9){
+            nextRow = row+1;
+            nextCol = 0 ;
+        }
+         if(sudo[row][col] != 0 ){
+             return SudokuSolver(sudo, nextRow , nextCol);
+         }
+
+        for( int digit  = 1 ; digit <= 9 ; digit++){
+            if( isSafe(sudo , row , col , digit)){
+                sudo[row][col] = digit;
+                if(SudokuSolver(sudo,nextRow , nextCol)){
+                    return true;
+                };
+                sudo[row][col] = 0 ;
             }
         }
         return false;
     }
 
-    public static boolean isSafe(char[][] board, int row, int col) {
-
-        //left diagonal
-        for( int i = row -1 , j = col -1 ; i >= 0 && j >= 0 ; i-- , j--){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
+    public static boolean isSafe(int[][]sudo , int row , int col , int digit){
+        // column wise
+        for( int i  = 0 ; i < 9  ; i++ ){
+            if(sudo[i][col] == digit) return false;
         }
 
-        // top diagonal
-        for( int i = row -1 ; i >= 0 ; i--){
-            if(board [i][ col] == 'Q'){
-                return false;
-            }
+        // row wise
+        for( int i  = 0 ; i < 9  ; i++ ){
+            if(sudo[row][i] == digit) return false;
         }
 
-        // right diagonla -> col++ row--
-        for( int i = row-1 , j= col+1 ; i >= 0 && j < board[0].length ; j++,i--){
-            if( board[i][j] == 'Q'){
-                return false;
+        //grid chek
+        int sr = (row/3)*3;
+        int sc = (col/3)*3;
+
+        for( int i = sr ; i < sr+3 ; i++){
+            for( int j = sc  ; j < sc+3 ; j++){
+                if(sudo[i][j] == digit) return false;
             }
         }
 
@@ -55,11 +64,18 @@ public class Revision2 {
     }
 
     public static void main(String[] args) {
-        char[][] board = {{'X', 'X', 'X', 'X'},
-                {'X', 'X', 'X', 'X'},
-                {'X', 'X', 'X', 'X'},
-                {'X', 'X', 'X', 'X'}};
+        int [][] sudo = {
+                {0, 0, 8, 0, 0, 0, 0, 0, 0},
+                {4, 9, 0, 1, 5, 7, 0, 0, 2},
+                {0, 0, 3, 0, 0, 4, 1, 9, 0},
+                {1, 8, 5, 0, 6, 0, 0, 2, 0},
+                {0, 0, 0, 0, 2, 0, 0, 6, 0},
+                {9, 6, 0, 4, 0, 5, 3, 0, 0},
+                {0, 3, 0, 0, 7, 2, 0, 0, 4},
+                {0, 4, 9, 0, 3, 0, 0, 5, 7},
+                {8, 2, 7, 0, 0, 9, 0, 1, 3}
+        };
 
-        nQueens(0 , board);
+        SudokuSolver(sudo,0,0);
     }
 }
