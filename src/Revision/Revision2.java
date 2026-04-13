3ea8beebd01e473cfd085ae785cd81b408060d25
Sahib <sahib.ptu@gmail.com>
2026-04-13 12:56:56 +0530

    Revsion of Search technique in Linked List

--- src/Revision/Revision2.java
+++ src/Revision/Revision2.java
@@ -112,6 +112,38 @@ public class Revision2 {
         size--;
     }
 
+    public void iterativeSearch(int data){
+        if(head == null){
+            System.out.println("LL is Empty");
+            return;
+        }
+        int idx = 1 ;
+        Node temp = head ;
+        while(temp != null){
+            if(data == temp.data){
+                System.out.println("Element found at index "+idx);
+                return;
+            }
+            temp = temp.next;
+            idx++;
+        }
+        System.out.println("Element not found");
+    }
+
+    public void recursiveSearch( Node head ,int data , int idx ) {
+        if(head == null){
+            System.out.println("Element not found");
+            return;
+        }
+
+        if(head.data == data){
+            System.out.println("Element found at index "+ idx);
+            return;
+        }
+
+        recursiveSearch(head.next , data , idx+1);
+    }
+
     public static void main(String[] args) {
         Revision2 ll = new Revision2();
         ll.addFirst(3);
@@ -121,6 +153,8 @@ public class Revision2 {
         ll.printLL();
         ll.addInMiddle(10, 3);
         ll.printLL();
+        ll.iterativeSearch(10);
+        ll.recursiveSearch(head , 10,1);
 
     }
 }
