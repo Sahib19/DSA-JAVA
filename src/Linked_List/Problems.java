920266a00c7111c6ee3fe8920a1d80a4e8ea644b
Sahib <sahib.ptu@gmail.com>
2026-04-15 01:54:15 +0530

    Add element at bottom of Stack

--- src/Linked_List/Problems.java
+++ src/Linked_List/Problems.java
@@ -1,76 +1,28 @@
 package Linked_List;
-
+import java.util.*;
 
 public class Problems {
-
-    public static class ListNode {
-        int data;
-        ListNode next;
-
-        ListNode(int data) {
-            this.data = data;
-        }
-    }
-
-    public ListNode head;
-    public ListNode tail;
-
-    // ✅ Function to add node at first
-    public void addFirst(int data) {
-
-        ListNode newNode = new ListNode(data);
-
-        // Case 1: If list is empty
-        if (head == null) {
-            head = tail = newNode;
+    public static void addAtBottom(int data , Stack <Integer> st){
+        if(st.isEmpty()){
+            st.push(data);
             return;
         }
 
-        // Case 2: If list already has elements
-        newNode.next = head;
-        head = newNode;
+        int val = st.pop();
+        addAtBottom(data,st);
+        st.push(val);
+
     }
-
-    // ✅ Function to print linked list
-    public void printList() {
-
-        if (head == null) {
-            System.out.println("Linked List is empty");
-            return;
-        }
-
-        ListNode temp = head;
-
-        while (temp != null) {
-            System.out.print(temp.data + " -> ");
-            temp = temp.next;
-        }
-
-        System.out.println("null");
-    }
-
     public static void main(String[] args) {
+        Stack <Integer> st = new Stack <>();
+        st.push(4);
+        st.push(3);
+        st.push(2);
+        st.push(1);
+        System.out.println(st);
+        addAtBottom(10,st);
+        System.out.println(st);
 
-        Problems list = new Problems();
-
-        list.addFirst(3);
-        list.addFirst(2);
-        list.addFirst(1);
-
-        list.printList();
-
-        Problems list2 = new Problems();
-        list2.addFirst(8);
-        list2.addFirst(6);
-        list2.addFirst(5);
-        list2.printList();
-
-        Problems list3 = new Problems();
-        list3.addFirst(15);
-        list3.addFirst(13);
-        list3.addFirst(9);
-        list3.printList();
-        System.out.println(list.head.data);
 
 
     }
