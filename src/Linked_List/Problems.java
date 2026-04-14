1061f5628b8e81e787d0e4833dfa537f6a4a1d81
Sahib <sahib.ptu@gmail.com>
2026-04-15 02:00:35 +0530

    Revrse String using Stack

--- src/Linked_List/Problems.java
+++ src/Linked_List/Problems.java
@@ -2,26 +2,19 @@ package Linked_List;
 import java.util.*;
 
 public class Problems {
-    public static void addAtBottom(int data , Stack <Integer> st){
-        if(st.isEmpty()){
-            st.push(data);
-            return;
+    public static String reverseString(String str , Stack <Character> st ){
+        if(str.isEmpty()){
+            return "";
         }
 
-        int val = st.pop();
-        addAtBottom(data,st);
-        st.push(val);
-
+        char curr = str.charAt(0);
+        st.push(curr);
+        return reverseString(str.substring(1) , st) + st.pop();
     }
     public static void main(String[] args) {
-        Stack <Integer> st = new Stack <>();
-        st.push(4);
-        st.push(3);
-        st.push(2);
-        st.push(1);
-        System.out.println(st);
-        addAtBottom(10,st);
-        System.out.println(st);
+        Stack <Character> st = new Stack <>();
+        System.out.println(reverseString("sahib" ,st));
+
 
 
 
