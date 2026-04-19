dada7373af872e8000c50c46eef3848d645b8c0f
Sahib <sahib.ptu@gmail.com>
2026-04-19 15:47:39 +0530

    Stock Span Problem

--- src/Linked_List/Problems.java
+++ src/Linked_List/Problems.java
@@ -2,33 +2,34 @@ package Linked_List;
 import java.util.*;
 
 public class Problems {
-    public static void bottomAdd(Stack <Integer> st , int  ele){
-        if(st.isEmpty()){
-            st.push(ele);
-            return;
+    public static void stockSpan (int [] stocks){
+        Stack < Integer> st = new Stack<>();
+        ArrayList <Integer> res = new ArrayList<>();
+        res.add(1);
+        st.push(0);
+
+        for( int i = 1 ; i < stocks.length ; i++){
+            int curr = stocks[i];
+            while(!st.isEmpty() && stocks[st.peek()] <= curr ){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                res.add(i+1);
+            }else {
+                res.add(i - st.peek());
+            }
+            st.push(i);
         }
 
-        int curr = st.pop();
-        bottomAdd(st ,  ele) ;
-        st.push(curr);
+        System.out.println(res);
     }
 
-    public static void reverseStack(Stack <Integer> st){
-        if(st.isEmpty()){
-            return;
-        }
-        int curr = st.pop();
-        reverseStack(st);
-        bottomAdd(st,curr);
-    }
     public static void main(String[] args) {
         Stack <Integer> st = new Stack <>();
-        st.push(1);
-        st.push(2);
-        st.push(3);
-        System.out.println(st);
-        reverseStack(st);
-        System.out.println(st);
+        int [] stocks = {100,80,60,70,60,85,100};
+        stockSpan(stocks);
+
+
 
 
 
