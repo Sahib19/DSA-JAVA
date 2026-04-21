7d46d265d376cf6ee2fdf5d646a807fcad055619
Sahib <sahib.ptu@gmail.com>
2026-04-21 22:11:35 +0530

    Stack problems Revision

--- src/Linked_List/Problems.java
+++ src/Linked_List/Problems.java
@@ -1,71 +1,62 @@
 package Linked_List;
-
 import java.util.*;
 
 public class Problems {
-    public static void printArr(int [] arr){
-        for( int ele : arr){
-            System.out.print(ele + " ");
+    public static class Node {
+        char data;
+        Node next;
+
+        Node (char data){
+            this.data = data;
+            this.next = null;
         }
     }
 
-    public static int[] rightMin(Stack <Integer> st, int  [] arr){
-        int [] rightMin = new int [arr.length];
-        for( int i =  arr.length-1 ; i>= 0 ; i--){
-            int curr = arr[i];
-            while(!st.isEmpty() && curr <= arr[st.peek()]){
-                st.pop();
-            }
-            if(st.isEmpty()){
-                rightMin[i] = arr.length;
-            }else{
-                rightMin[i] = st.peek();
-            }
-            st.push(i);
+    public static Node head;
+
+    public void printLL(){
+        Node temp =  head ;
+        while (temp != null){
+            System.out.print(temp.data+" -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void checkPalindrome(){
+        if(head == null){
+            System.out.println("LL is empty");
         }
 
-        return rightMin;
-    }
-
-    public static int[] leftMin(Stack <Integer> st, int  [] arr){
-        int [] leftMin = new int [arr.length];
-        for( int i = 0 ; i<  arr.length; i++){
-            int curr = arr[i];
-            while(!st.isEmpty() && arr[st.peek()] >= curr){
-                st.pop();
-            }
-            if(st.isEmpty()){
-                leftMin[i] = -1;
-            }else{
-                leftMin[i] = st.peek();
-            }
-            st.push(i);
+        Stack <Character> st = new Stack <>();
+        Node temp = head ;
+        while(temp != null){
+            st.push(temp.data);
+            temp =  temp.next ;
         }
 
-        return leftMin;
-    }
-
-    public static void MaxArea( int [] arr){
-        Stack <Integer> s1 = new Stack <>();
-        Stack <Integer> s2 = new Stack <>();
-        int [] leftMin = leftMin(s1 , arr);
-        int [] rightMin = rightMin(s2 , arr);
-        int maxArea = Integer.MIN_VALUE;
-        for( int i = 0 ; i < arr.length ; i++){
-            int curr = arr[i] * (rightMin[i]-leftMin[i]-1);
-            maxArea = Math.max(maxArea, curr);
+        temp = head ;
+        while(temp != null){
+            if(temp.data != st.pop()){
+                System.out.println("Not a Palindrome");
+                return;
+            }
+            temp = temp.next;
         }
 
-        System.out.println("The Max Area is " + maxArea);
+        System.out.println("Palindrome");
     }
-
-
-    public static void main(String[] args) {
-        int [] arr = {2,1,5,6,2,3};
-        Stack<Integer> st = new Stack<>();
-        MaxArea(arr);
-
-
-
-    }
+     public static void main(String [] args){
+        Problems ll = new Problems ();
+        Node n1 = new Node ('s');
+        head = n1 ;
+        Node n2 = new Node ('a');
+        Node n3 = new Node ('z');
+        Node n4 = new Node ('s');
+        n1.next = n2 ;
+        n2.next = n3 ;
+        n3.next = n4 ;
+        ll.printLL();
+        ll.checkPalindrome();
+     }
 }
