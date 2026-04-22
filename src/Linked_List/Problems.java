45e0e5a6f0622a5f752a564bd01eea2662eb4ada
Sahib <sahib.ptu@gmail.com>
2026-04-22 23:17:25 +0530

    Decode a String

--- src/Linked_List/Problems.java
+++ src/Linked_List/Problems.java
@@ -3,46 +3,49 @@ package Linked_List;
 import java.util.*;
 
 public class Problems {
+    public static String multiply(String str , int num){
+        String temp = "" ;
+        for(int i = 1 ; i <= num ; i++){
+            temp += str ;
+        }
+        return temp;
+    }
 
-    public static void simplyfyPath(String str) {
-        Stack<String> st = new Stack<>();
-        int i = 0;
-        StringBuilder sb = new StringBuilder();
-        while (i < str.length()) {
-            // skipping the '/'
-            while (i < str.length() && str.charAt(i) == '/') {
-                i++;
+    public static String decode(String str){
+        Stack <Integer> numStack = new Stack <>();
+        Stack <String> characterStack = new Stack <>();
+
+        String curr = "";
+        int num = 0;
+        for(char ch : str.toCharArray()){
+
+            if(Character.isDigit(ch)){
+                num = num*10 + ch-'0';
             }
-
-            // now extracting the main thing
-            while (i < str.length() && str.charAt(i) != '/') {
-                sb.append(str.charAt(i));
-                i++;
+            else if(ch == '['){
+                numStack.push(num);
+                characterStack.push(curr);
+                num = 0 ;
+                curr = "";
             }
+            else if(ch == ']'){
+                int times = numStack.pop();
+                String prev = characterStack.pop();
 
-            String res = sb.toString();
-            sb.setLength(0);
+                String res = multiply(curr ,times);
+                System.out.println(res);
+                System.out.println(prev);
+                curr = prev + res ;
 
-            if (res.isEmpty() || res.equals(".")) {
-                continue;
-            } else if (res.equals("..")) {
-                if(!st.isEmpty()){
-                    st.pop();
-                }
             }else{
-                st.push(res);
+                curr += ch ;
             }
         }
 
-        sb.setLength(0);
-        for (String s : st) {
-            sb.append('/').append(s);
-        }
-
-        System.out.println(sb.toString());
+        return curr;
     }
 
     public static void main(String[] args) {
-        simplyfyPath("/home/user/../docs");
+        System.out.println(decode("ab3[ca]"));
     }
 }
