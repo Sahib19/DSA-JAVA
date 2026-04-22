1afffb888961eee1b6dfd937dc25dd29d3bf3d86
Sahib <sahib.ptu@gmail.com>
2026-04-23 00:32:24 +0530

    Trapping rain water problem using Stack

--- src/Linked_List/Problems.java
+++ src/Linked_List/Problems.java
@@ -3,49 +3,42 @@ package Linked_List;
 import java.util.*;
 
 public class Problems {
-    public static String multiply(String str , int num){
-        String temp = "" ;
-        for(int i = 1 ; i <= num ; i++){
-            temp += str ;
-        }
-        return temp;
-    }
 
-    public static String decode(String str){
-        Stack <Integer> numStack = new Stack <>();
-        Stack <String> characterStack = new Stack <>();
+    public static int trp(int [] heights){
+        Stack <Integer> st = new Stack <>();
+        int water = 0 ;
+        int bottom = 0;
+        for( int i = 0 ; i < heights.length ; i++){
+            int curr = heights[i];
 
-        String curr = "";
-        int num = 0;
-        for(char ch : str.toCharArray()){
+            while(!st.isEmpty() && curr > heights[st.peek()]) {
+                bottom = st.pop();
+                if (st.isEmpty()) {
+                    break;
+                }
 
-            if(Character.isDigit(ch)){
-                num = num*10 + ch-'0';
+
+                int left = st.peek();
+                int right = i;
+
+                int width = right - left - 1;
+
+                int height = Math.min(heights[left], heights[right]) - heights[bottom];
+
+                water += width * height;
+
             }
-            else if(ch == '['){
-                numStack.push(num);
-                characterStack.push(curr);
-                num = 0 ;
-                curr = "";
-            }
-            else if(ch == ']'){
-                int times = numStack.pop();
-                String prev = characterStack.pop();
-
-                String res = multiply(curr ,times);
-                System.out.println(res);
-                System.out.println(prev);
-                curr = prev + res ;
-
-            }else{
-                curr += ch ;
-            }
+            st.push(i);
         }
 
-        return curr;
-    }
+        return water;
 
-    public static void main(String[] args) {
-        System.out.println(decode("ab3[ca]"));
+    }
+    public static void main(String []  args){
+        // Trapping Rain Water Problem
+        int [] level = {6,3,2,4};
+        System.out.println(trp(level));
+
+
     }
 }
