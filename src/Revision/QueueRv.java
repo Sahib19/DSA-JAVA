612f670be224f202a0b1da26802058640dddcdd9
Sahib <sahib.ptu@gmail.com>
2026-04-25 03:12:09 +0530

    Interleaving of Queue

--- src/Revision/QueueRv.java
+++ src/Revision/QueueRv.java
@@ -3,25 +3,23 @@ package Revision;
 import java.util.*;
 
 public class QueueRv {
-
-    public static void nrp( String str){
-        int [] freq = new int [26] ; // 0 to 25
-        Queue <Character> q1 = new ArrayDeque<>();
-        for( int i = 0 ; i < str.length() ; i++){
-            char curr = str.charAt(i);
-            freq[curr-'a']++;
-            q1.add(curr);
-            while(!q1.isEmpty() && freq[q1.peek()-'a'] > 1){
-                q1.remove(); // element get removed from the front
-            }
-            if(q1.isEmpty()){
-                System.out.print("-1 " );
-            }else{
-                System.out.print(q1.peek()+" ");
-            }
-        }
-    }
     public static void main(String [] args){
-        nrp("aabccxb");
+        Queue <Integer> q1 = new ArrayDeque(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        Queue  <Integer> q2 = new ArrayDeque();
+
+        int len = q1.size()/2;
+       while(q1.size() != len){
+           q2.add(q1.remove());
+       }
+        System.out.println(q2);
+
+        while(!q2.isEmpty()){
+           q1.add(q2.remove());
+           q1.add(q1.remove());
+       }
+
+       System.out.println(q1);
+
+
     }
 }
