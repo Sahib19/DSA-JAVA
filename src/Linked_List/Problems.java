5e0c15df19db6c68b67fcef139e71149f5da9b24
Sahib <sahib.ptu@gmail.com>
2026-04-24 14:36:36 +0530

    Queue implementation using Arrays

--- src/Linked_List/Problems.java
+++ src/Linked_List/Problems.java
@@ -1,43 +1,7 @@
 package Linked_List;
 
-import java.util.*;
-
 public class Problems {
-
-    public static int trp(int [] heights){
-        Stack <Integer> st = new Stack <>();
-        int water = 0 ;
-        int bottom = 0;
-        for( int i = 0 ; i < heights.length ; i++){
-            int curr = heights[i];
-
-            while(!st.isEmpty() && curr > heights[st.peek()]) {
-                bottom = st.pop();
-                if (st.isEmpty()) {
-                    break;
-                }
-
-
-                int left = st.peek();
-                int right = i;
-
-                int width = right - left - 1;
-
-                int height = Math.min(heights[left], heights[right]) - heights[bottom];
-
-                water += width * height;
-
-            }
-            st.push(i);
-        }
-
-        return water;
-
-    }
     public static void main(String []  args){
-        // Trapping Rain Water Problem
-        int [] level = {6,3,2,4};
-        System.out.println(trp(level));
 
 
     }
