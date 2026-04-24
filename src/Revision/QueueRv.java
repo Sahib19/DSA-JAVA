d46b1d9a73cb46598822db36f1dcf37142393e80
Sahib <sahib.ptu@gmail.com>
2026-04-24 23:20:04 +0530

    Implement Queue using 2 stack

--- src/Revision/QueueRv.java
+++ src/Revision/QueueRv.java
@@ -3,34 +3,45 @@ package Revision;
 import java.util.*;
 
 public class QueueRv {
+    Stack <Integer> s1 = new Stack<>(); // adding purpose
 
-    public static Stack <Integer> st = new Stack <>();
+    Stack <Integer> s2 = new Stack<>(); // removing purpose
+    int front = -1 ;
+    int rear = -1 ;
+    int size = 0 ;
 
-    public void add(int data){
-        if(st.isEmpty()){
-            st.push(data);
-            return;
-        }
-        int top = st.pop();
-        add(data);
-        st.push(top);
+    public boolean isEmpty(){
+        return s1.isEmpty() && s2.isEmpty();
     }
 
-    public void remove(){
-        if(st.isEmpty()){
-            System.out.println("Queue is Empty");
-            return;
+    public void add( int data){ // O(1)
+        s1.add(data);
+    }
+
+    public int remove(){ // O(n)
+        if(isEmpty()){
+            System.out.println("Queue is Empty !!!!!");
+            return -1;
         }
 
-        int ele = st.pop();
-        System.out.println(ele + " removed ");
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
     }
+
+
+
     public static void main(String[] args) {
         QueueRv q1 = new QueueRv();
         q1.add(1);
         q1.add(2);
         q1.add(3);
-        q1.remove();
+
+        System.out.println(q1.remove());
+        System.out.println(q1.remove());
 
     }
 }
