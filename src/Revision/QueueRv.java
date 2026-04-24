9f2af926dbdcb038e904ee84b348704a5ed36ccf
Sahib <sahib.ptu@gmail.com>
2026-04-25 02:55:26 +0530

    First Non repeating character in a String

--- src/Revision/QueueRv.java
+++ src/Revision/QueueRv.java
@@ -3,45 +3,25 @@ package Revision;
 import java.util.*;
 
 public class QueueRv {
-    Stack <Integer> s1 = new Stack<>(); // adding purpose
 
-    Stack <Integer> s2 = new Stack<>(); // removing purpose
-    int front = -1 ;
-    int rear = -1 ;
-    int size = 0 ;
-
-    public boolean isEmpty(){
-        return s1.isEmpty() && s2.isEmpty();
-    }
-
-    public void add( int data){ // O(1)
-        s1.add(data);
-    }
-
-    public int remove(){ // O(n)
-        if(isEmpty()){
-            System.out.println("Queue is Empty !!!!!");
-            return -1;
-        }
-
-        if(s2.isEmpty()){
-            while(!s1.isEmpty()){
-                s2.push(s1.pop());
+    public static void nrp( String str){
+        int [] freq = new int [26] ; // 0 to 25
+        Queue <Character> q1 = new ArrayDeque<>();
+        for( int i = 0 ; i < str.length() ; i++){
+            char curr = str.charAt(i);
+            freq[curr-'a']++;
+            q1.add(curr);
+            while(!q1.isEmpty() && freq[q1.peek()-'a'] > 1){
+                q1.remove(); // element get removed from the front
+            }
+            if(q1.isEmpty()){
+                System.out.print("-1 " );
+            }else{
+                System.out.print(q1.peek()+" ");
             }
         }
-        return s2.pop();
     }
-
-
-
-    public static void main(String[] args) {
-        QueueRv q1 = new QueueRv();
-        q1.add(1);
-        q1.add(2);
-        q1.add(3);
-
-        System.out.println(q1.remove());
-        System.out.println(q1.remove());
-
+    public static void main(String [] args){
+        nrp("aabccxb");
     }
 }
