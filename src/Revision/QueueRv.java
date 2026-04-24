32bf70e927d2728f59d8da5f2225261e7ddfeab2
Sahib <sahib.ptu@gmail.com>
2026-04-24 18:01:38 +0530

    Queue Implementation using single Stack + Recursion

--- src/Revision/QueueRv.java
+++ src/Revision/QueueRv.java
@@ -1,90 +1,36 @@
 package Revision;
 
-class Queue {
-    int[] Queue;
-    int n; // total size of Queue that is Allocated
-    int size; // it tells how much element is currently present in the queue
-    int front;
-    int rear;
-
-    Queue(int n) {
-        this.n = n;
-        Queue = new int[n];
-        size = 0;
-        front = -1;
-        rear = -1;
-    }
-
-    public void add(int data) {
-        if (size == n) {
-            System.out.println("Queue is full");
-            return;
-        }
-        if (front == -1) { // queue is Empty
-            Queue[++front] = data;
-            rear++;
-            size++;
-            return;
-        }
-        rear = rear + 1;
-        Queue[rear] = data;
-        size++;
-    }
-
-    public void remove() {
-        if (front == -1) {
-            System.out.println("Queue is Empty");
-            return;
-        } else {
-            int res = Queue[front];
-            if (front == rear) {
-                front = rear = -1;
-                System.out.println(res);
-                return;
-            } else {
-                for (int i = 1; i < size; i++) {
-                    Queue[i - 1] = Queue[i];
-                }
-                rear--;
-                System.out.println(res);
-            }
-            size--;
-        }
-
-    }
-
-    public boolean isEmpty() {
-        return front == -1;
-    }
-
-    public void pQ() {
-        if (size == 0) {
-            System.out.println("Queue is Empty");
-            return;
-        }
-        int temp = front;
-        System.out.print("Front -> ");
-        while (temp < size) {
-            System.out.print(Queue[temp] + " - ");
-            temp++;
-        }
-        System.out.println(" <-- rear");
-    }
-}
-
+import java.util.*;
 
 public class QueueRv {
+
+    public static Stack <Integer> st = new Stack <>();
+
+    public void add(int data){
+        if(st.isEmpty()){
+            st.push(data);
+            return;
+        }
+        int top = st.pop();
+        add(data);
+        st.push(top);
+    }
+
+    public void remove(){
+        if(st.isEmpty()){
+            System.out.println("Queue is Empty");
+            return;
+        }
+
+        int ele = st.pop();
+        System.out.println(ele + " removed ");
+    }
     public static void main(String[] args) {
-        Queue q1 = new Queue(5);
+        QueueRv q1 = new QueueRv();
         q1.add(1);
         q1.add(2);
         q1.add(3);
-        q1.pQ();
-        q1.add(4);
-        q1.pQ();
         q1.remove();
-        q1.remove();
-        q1.pQ();
 
     }
 }
