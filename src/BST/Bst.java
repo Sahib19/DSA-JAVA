29374c0b1b882531625b19e8e33692a7e5a56fa9
Sahib <sahib.ptu@gmail.com>
2026-05-15 09:41:34 +0530

    Delete Node in a BST

--- src/BST/Bst.java
+++ src/BST/Bst.java
@@ -32,14 +32,56 @@ public class Bst {
         System.out.print(root.data+" ");
         inorder(root.right);
     }
+    public static Node deleteNode(Node root , int val){ // val is the
+        // value that we want to delete from the tree
+        if( root == null){
+            // no such node exist;
+            return null;
+        }
+        if( root.data > val){
+            root.left  =  deleteNode(root.left , val);
+        }else if(root.data < val){
+            root.right = deleteNode(root.right , val);
+        }else{
+            // root.data == val -> issi node ko delete krna hai
 
+            // Case 1 -> Node have no child
+            if(root.left == null && root.right == null){
+                return null;
+            }
+
+            // Case 2 -> Node have one child
+            if(root.left == null){
+                return root.right;
+            }else if ( root.right == null){
+                return root.left;
+            }
+
+            // Case 3 -> Having two child
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = deleteNode(root.right , IS.data);
+
+        }
+
+        return root;
+    }
+
+    public static Node findInorderSuccessor(Node root){
+        while( root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
     public static void main (String [] args){
-        int [] values = {5,9,6,1,4,6,2};
+        int [] values = {5,9,6,1,4,2, 15,14,0};
         Node root = null ;
         for( int i = 0  ; i < values.length ; i++){
             root = createBST(root , values[i]);
         }
 
+        inorder(root); System.out.println();
+        deleteNode(root,41);
         inorder(root);
 
     }
